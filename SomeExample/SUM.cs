acc4ba501abbd57ed031a1cc59d55d5465d6c9ba
Martin <martin.ruzhinov@gmail.com>
2015-01-14 13:49:37 +0200

    Something
    Example of a simple program

---
+++ SomeExample/SUM.cs
@@ -0,0 +1,24 @@
+﻿using System; // използване на namespace(библиотека)
+
+
+namespace SumNumbers
+{
+    class Program // клас- моделиране на неща с подобни признаци (каквото и да е има на клас)
+    {
+        static void Main()
+        {    // мястото където стартираме нашата програма        
+
+            int m;
+            string baseTextValue=Console.ReadLine();
+            m = int.Parse(baseTextValue);
+
+            int n;
+            string powerTextValue=Console.ReadLine();
+            n = int.Parse(powerTextValue) ;
+
+            ;
+            Console.WriteLine(Math.Pow(m, n));
+
+        }
+    }
+}
